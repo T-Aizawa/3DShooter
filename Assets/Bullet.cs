d6873759cf084fad77b6e65564a108dfd9a6ff6c
T-Aizawa <aizawa.takamichi@gmail.com>
2022-09-28 18:55:09 +0900

    [update]弾を時間で消えるように変更
    消失時間の追加と設定、Wallタグの追加と設定、衝突時の消える処理の変更

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -6,10 +6,13 @@ public class Bullet : MonoBehaviour
 {
     private Rigidbody bulletRb;
     public float bulletSpeed;
+    public float lifetime;
 
     void Start()
     {
         bulletRb = this.GetComponent<Rigidbody>();
+        // 時間経過で消滅する
+        Destroy(this.gameObject, lifetime);
     }
 
     void Update()
@@ -19,13 +22,13 @@ public class Bullet : MonoBehaviour
     }
 
     /// <summary>
-    /// オブジェクトと衝突した時に実行される
+    /// オブジェクトと衝突した時に実行
     /// </summary>
     /// <param name="other">衝突したオブジェクト</param>
     void OnCollisionEnter(Collision other)
     {
-        // 衝突したのがプレイヤーか弾でないなら消滅する
-        if (other.gameObject.tag != "Player" || other.gameObject.tag != "Bullet"){
+        // 壁に衝突したら消滅する
+        if (other.gameObject.tag == "Wall"){
             Destroy(this.gameObject);
         }
     }
