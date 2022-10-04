06a99a816133b3e6cebdd2c9a31211d85cfc8c02
T-Aizawa <aizawa.takamichi@gmail.com>
2022-10-04 19:34:53 +0900

    [fix]文言修正
    CreateメニューにCreateの文言はいらないや

--- Assets/Data/Enemy/EnemyData.cs
+++ Assets/Data/Enemy/EnemyData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "EnemyData", menuName = "MyScriptable/Create EnemyData")]
+[CreateAssetMenu(fileName = "EnemyData", menuName = "MyScriptable/EnemyData")]
 public class EnemyData : ScriptableObject
 {
     // status
